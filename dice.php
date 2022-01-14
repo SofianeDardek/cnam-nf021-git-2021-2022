b35468c67e1cbdcba0bfb6d2f62b4d39f7f888b1
sofiane dardek <sofiane.dardek@hotmail.fr>
2022-01-14 10:35:13 +0100

    Amélioration du jeu de dé
    (Bob)

--- dice.php
+++ dice.php
@@ -2,6 +2,6 @@
 
 $n = random_int(1, 6);
 $n2 = random_int(1, 6);
-$n2 = random_int(1, 6);
+$n3 = random_int(1, 6);
 
-echo "Vous avez tiré un $n<br>\n";
+echo "Vous avez tiré un $n un $n2 et un $n3 <br>\n";
